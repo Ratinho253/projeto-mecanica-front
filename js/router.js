f84fe44603422960ce5810abafe9ec1ae7ec2e36
Ratinho253 <jv14700@gmail.com>
2023-06-09 14:28:09 -0300

    fazendo

---
+++ js/router.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const routes = {
+    '/' : '/all_professor',
+    '/' : '/pages/vermelho.html',
+    '/azul' : '/pages/azul.html',
+    '/verde' : '/pages/green.html',
+    '/tomato' : '/pages/tomato.html',
+}
+
+const route = async () => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+    console.log();
+
+    const response = await fetch(routes[path])
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+}
+
+window.route = route
